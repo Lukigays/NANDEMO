9ff633a52dacfeb00358b6158d349d1271600e3f
lukigays <luki85030@gmail.com>
2025-01-01 19:36:42 +0700

    https://github.com/Lukigays/nandemo

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7514660413:AAHpqFtyfVj3qArPaXbcgr9JKM04uBxEjLg", // Token bot Telegram
+  OWNER_ID: "5754537788", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
